065c2ed6cd24dcce37d4c081becb40206fd82617
Anuraag Agrawal <rag@istellar.jp>
2018-05-30 17:12:59 +0900

    Add a fake server and client to allow easy experimenting with API stub and demonstrate simple code usage.

---
+++ examples/fakeserver/README.md
@@ -0,0 +1,15 @@
+# StellarStation API Fakeserver
+
+A fake implementation of the StellarStation API to use to check behavior of client code before
+integrating with the service.
+
+## Features
+
+- Verifies a fake API key
+- Returns 1MB of random telemetry every second
+- Echos back received commands
+- Cancels the connection after 5 minutes
+
+## Running
+
+// TODO(rag): Add instructions for running with docker after pushing an image.
---
+++ examples/fakeserver/src/main/java/com/stellarstation/api/fakeserver/FakeServerMain.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2018 Infostellar, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.stellarstation.api.fakeserver;
+
+import com.linecorp.armeria.server.Server;
+import com.linecorp.armeria.server.ServerBuilder;
+import com.linecorp.armeria.server.auth.HttpAuthServiceBuilder;
+import dagger.Component;
+import dagger.Module;
+import dagger.Provides;
+import dagger.multibindings.IntoSet;
+import java.util.function.Consumer;
+import javax.inject.Singleton;
+import org.curioswitch.common.server.framework.ServerModule;
+import org.curioswitch.common.server.framework.grpc.GrpcServiceDefinition;
+
+public class FakeServerMain {
+
+  @Module(includes = ServerModule.class)
+  abstract static class FakeServerModule {
+
+    @Provides
+    @IntoSet
+    static GrpcServiceDefinition service(
+        FakeStellarStationService service, FakeApiKeyAuthorizer authorizer) {
+      return new GrpcServiceDefinition.Builder()
+          .addServices(service)
+          .decorator(new HttpAuthServiceBuilder().addOAuth2(authorizer).newDecorator())
+          .path("/")
+          .build();
+    }
+
+    @Provides
+    @IntoSet
+    static Consumer<ServerBuilder> serverCustomizer() {
+      return sb -> sb.http(8081);
+    }
+
+    private FakeServerModule() {}
+  }
+
+  @Component(modules = FakeServerModule.class)
+  @Singleton
+  interface FakeServerComponent {
+    Server server();
+  }
+
+  public static void main(String[] args) {
+    DaggerFakeServerMain_FakeServerComponent.create().server();
+  }
+
+  private FakeServerMain() {}
+}
