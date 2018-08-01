be8eeb2ab2df0ef878f91ed1f73b3bd401bb83cb
Anuraag Agrawal <anuraaga@gmail.com>
2018-08-01 18:55:16 +0900

    Release 0.0.8 (#27)

--- stubs/python/src/misc/python/setup.py
+++ stubs/python/src/misc/python/setup.py
@@ -20,7 +20,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='stellarstation',
-    version='0.0.7',
+    version='0.0.8',
     author='StellarStation API Team',
     author_email='stellarstation-api-team@istellar.jp',
     description='Client stubs for accessing the StellarStation API',
