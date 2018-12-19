eca0d34983b98cf53971718e21cf9373eeabbd30
Ryuichi Hoshi <ryuichi@istellar.jp>
2018-12-19 17:13:54 +0900

    Renamed the directory name from test to integration-tests.

--- integration-tests/python/conn/stub_test.py
+++ integration-tests/python/conn/stub_test.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-"""The test for gRPC stubs for StellarStation API."""
+"""The integration-tests for gRPC stubs for StellarStation API."""
 
 
 def test_satellite_service_stub(stub_factory):
