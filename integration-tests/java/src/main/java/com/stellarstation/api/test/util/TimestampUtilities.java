f10a6785c059001173a72b36b5166b148e6754d6
Ryuichi Hoshi <ryuichi@istellar.jp>
2019-06-24 15:27:21 +0900

    Added pass and plan tests (#164)
    * Added pass and plan tests
    
    * Removed conversions to ImmutableList.
    
    * Changed LocalDateTime to Instant.
    
    * Added a note for aos/current time test.
    
    * Style fix

---
+++ integration-tests/java/src/main/java/com/stellarstation/api/test/util/TimestampUtilities.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2019 Infostellar, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.stellarstation.api.test.util;
+
+import com.google.protobuf.Timestamp;
+import com.google.protobuf.util.Timestamps;
+import java.time.Instant;
+
+public class TimestampUtilities {
+
+  public static Timestamp fromInstant(Instant instant) {
+    return Timestamp.newBuilder()
+        .setSeconds(instant.getEpochSecond())
+        .setNanos(instant.getNano())
+        .build();
+  }
+
+  public static Instant toInstant(Timestamp timestamp) {
+    return Instant.ofEpochSecond(Timestamps.toSeconds(timestamp), timestamp.getNanos());
+  }
+
+  private TimestampUtilities() {}
+}
