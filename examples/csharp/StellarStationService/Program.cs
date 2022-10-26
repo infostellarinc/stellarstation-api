4e7d029b8d297909642c626fb41ad2b9ccdf2c2c
Camille Chomel <camille.chomel@gmail.com>
2022-10-26 16:30:58 +0900

    in progress

--- examples/csharp/StellarStationService/Program.cs
+++ examples/csharp/StellarStationService/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Grpc.Auth;
+using Google.Apis.Auth.OAuth2;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+var credential = GoogleCredential.GetApplicationDefault();
+
+
+var newCreds = GoogleGrpcCredentials.ToChannelCredentials(credential);
+using var channel = GrpcChannel.ForAddress("https://localhost:7042", new GrpcChannelOptions
+    {
+        Credentials = newCreds,
+        MaxReceiveMessageSize = 5 * 1024 * 1024, // 5 MB
+        MaxSendMessageSize = 2 * 1024 * 1024 // 2 MB
+    });
+var client = new Stellarstation.Api.V1.StellarStationService.StellarStationServiceClient(channel);
+
+var startTime = DateTime.UtcNow - DateTime.MinValue;
+var start = new Google.Protobuf.WellKnownTypes.Timestamp{ Seconds = (long)startTime.TotalSeconds, };
+var stopTime = startTime.Add(TimeSpan.FromDays(3));
+var stop = new Google.Protobuf.WellKnownTypes.Timestamp{ Seconds = (long)startTime.TotalSeconds, };
+
+var req = new Stellarstation.Api.V1.ListPlansRequest {
+    SatelliteId = "174",
+    AosAfter = start,
+    AosBefore = stop,
+};
--- examples/csharp/StellarStationService/pull_dependencies.bat
+++ examples/csharp/StellarStationService/pull_dependencies.bat
@@ -1,3 +1,4 @@
 dotnet add StellarStationService.csproj package Grpc.Net.Client
 dotnet add StellarStationService.csproj package Google.Protobuf
 dotnet add StellarStationService.csproj package Grpc.Tools
+dotnet add StellarStationService.csproj package Microsoft.AspNetCore.Authentication.JwtBearer
