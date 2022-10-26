9b86a5ab6e557f00afe2e2c9f71373ca8139b6e4
Camille Chomel <camille.chomel@gmail.com>
2022-10-26 13:31:17 +0900

    compiling example

---
+++ examples/csharp/StellarStationService/pull_dependencies.bat
@@ -0,0 +1,3 @@
+dotnet add StellarStationService.csproj package Grpc.Net.Client
+dotnet add StellarStationService.csproj package Google.Protobuf
+dotnet add StellarStationService.csproj package Grpc.Tools
