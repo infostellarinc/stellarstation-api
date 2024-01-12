cdf5007482b15238457469644879bcb409133cc1
Ben Mitchell <dezyh@outlook.com>
2024-01-12 14:32:06 +0900

    feat: add reconnection logic and refactor

--- examples/rust/streamcli/src/stream.rs
+++ examples/rust/streamcli/src/stream.rs
@@ -1,20 +1,20 @@
-use crate::Args;
+use crate::{stream::api::stream_event::Event, Args};
 
 use google_cloud_auth::{
     credentials::CredentialsFile,
     project::{create_token_source_from_credentials, Config},
     token_source::TokenSource,
 };
-use std::{str::FromStr, time::Duration};
+use std::str::FromStr;
 use tokio::{select, sync::mpsc};
 use tokio_stream::wrappers::ReceiverStream;
 use tokio_util::sync::CancellationToken;
 use tonic::{
     metadata::MetadataValue,
     transport::{Channel, Endpoint},
-    Request,
+    Request, Streaming,
 };
-use tracing::{error, info};
+use tracing::{debug, error, info, warn};
 
 /// Include the rust code generated from the stellarstation-api protos by `prost`.
 /// In the future we will replace this with a crate hosted on cargo.
@@ -40,6 +40,9 @@ pub mod api {
     }
 }
 use api::stellar_station_service_client::StellarStationServiceClient;
+use api::{SatelliteStreamRequest, SatelliteStreamResponse};
+
+use self::api::satellite_stream_response::Response;
 
 async fn token_source(key: String, url: &str) -> anyhow::Result<Box<dyn TokenSource>> {
     let creds = CredentialsFile::new_from_file(key).await?;
@@ -53,104 +56,220 @@ async fn token_source(key: String, url: &str) -> anyhow::Result<Box<dyn TokenSou
     Ok(create_token_source_from_credentials(&creds, &config).await?)
 }
 
-struct StreamConfig {
-    channel: Channel,
-    bearer: String,
-    satellite: String,
+struct StreamResult {
+    /// Was all the expected telemetry received.
+    complete: bool,
+    /// How many total bytes have been received.
+    bytes: usize,
+    /// How many total frames have been received.
+    frames: usize,
+    /// The ID of the stream. Used to reconnect in the event of a disconnect.
+    stream_id: Option<String>,
+    /// The ID of the last message received on the stream. Used to correctly resume the stream from
+    /// the last message received by the client. This ensures exactly-once deliver of messages even
+    /// through reconnections.
+    stream_resume_id: Option<String>,
 }
 
-async fn one_stream(i: u16, ctx: CancellationToken, config: StreamConfig) -> anyhow::Result<()> {
-    let mut client = StellarStationServiceClient::new(config.channel)
-        .max_decoding_message_size(10 * 1024 * 1024)
-        .max_encoding_message_size(10 * 1024 * 1024);
+async fn stream_with_reconnect(
+    ctx: CancellationToken,
+    token: String,
+    client: StellarStationServiceClient<Channel>,
+    satellite: String,
+) -> anyhow::Result<StreamResult> {
+    let mut stream_results = StreamResult {
+        complete: false,
+        bytes: 0,
+        frames: 0,
+        stream_id: None,
+        stream_resume_id: None,
+    };
 
+    while !stream_results.complete {
+        let attempt_results = stream_attempt(
+            ctx.clone(),
+            token.clone(),
+            client.clone(),
+            satellite.clone(),
+            stream_results.stream_id.clone(),
+            stream_results.stream_resume_id.clone(),
+        )
+        .await?;
+
+        stream_results.complete = attempt_results.complete;
+        stream_results.stream_id = attempt_results.stream_id;
+        stream_results.stream_resume_id = attempt_results.stream_resume_id;
+        stream_results.bytes += attempt_results.bytes;
+        stream_results.frames += attempt_results.frames;
+    }
+
+    Ok(stream_results)
+}
+
+async fn stream_attempt(
+    ctx: CancellationToken,
+    token: String,
+    mut client: StellarStationServiceClient<Channel>,
+    satellite: String,
+    stream_id: Option<String>,
+    stream_resume_id: Option<String>,
+) -> anyhow::Result<StreamResult> {
     let (tx, rx) = mpsc::channel(1);
     let mut request = Request::new(ReceiverStream::new(rx));
 
-    // Use the OAuth2 bearer token generated from the StellarStation API key to authorize the
-    // stream.
-    request.metadata_mut().insert(
-        "authorization",
-        MetadataValue::from_str(&format!("Bearer {}", config.bearer))?,
-    );
+    request
+        .metadata_mut()
+        .insert("authorization", MetadataValue::from_str(&token)?);
 
     let response = client.open_satellite_stream(request).await?;
-    info!(i, "connected to stellarstation");
 
-    let rx_ctx = ctx.clone();
-    let rx_handle = tokio::spawn(async move {
-        let mut rx = response.into_inner();
+    stream_setup(&tx, satellite, stream_id, stream_resume_id).await;
 
-        loop {
-            select! {
-                msg = rx.message() => match msg {
-                    Ok(Some(msg)) => info!(?msg, "stream received message from server"),
-                    Ok(None) => {
-                        info!("stream closed gracefully by server");
-                        break;
-                    }
-                    Err(err) => {
-                        error!("stream closed by server with error: {:?}", err);
-                        break;
-                    }
+    let results = stream_receiver(ctx.clone(), response.into_inner()).await;
+
+    results
+}
+
+async fn stream_receiver(
+    ctx: CancellationToken,
+    mut rx: Streaming<SatelliteStreamResponse>,
+) -> anyhow::Result<StreamResult> {
+    let mut results = StreamResult {
+        complete: false,
+        bytes: 0,
+        frames: 0,
+        stream_id: None,
+        stream_resume_id: None,
+    };
+
+    loop {
+        select! {
+            msg = rx.message() => match msg {
+                Ok(Some(msg)) => {
+                    on_message(msg, &mut results);
                 },
-                _ = rx_ctx.cancelled() => {
-                    info!("received exit signal, client initiating stream close");
+                Ok(None) => {
+                    info!("stream closed gracefully by server");
                     break;
                 }
+                Err(err) => {
+                    error!("stream closed by server with error: {:?}", err);
+                    break;
+                }
+            },
+            _ = ctx.cancelled() => {
+                info!("received exit signal, client initiating stream close");
+                break;
             }
         }
-    });
+    }
 
-    let tx_ctx = ctx.clone();
-    let tx_handle = tokio::spawn(async move {
-        let initial_stream_request = api::SatelliteStreamRequest {
-            satellite_id: "333".into(),
-            ..Default::default()
-        };
+    Ok(results)
+}
 
-        match tx.send(initial_stream_request).await {
-            Ok(_) => info!("sent initial stream request"),
-            Err(err) => error!(?err, "failed to send initial stream request"),
-        };
+/// Send the initial configuration message on the stream to apply configuration options and
+/// filters.
+async fn stream_setup(
+    tx: &mpsc::Sender<SatelliteStreamRequest>,
+    satellite_id: String,
+    stream_id: Option<String>,
+    stream_resume_id: Option<String>,
+) {
+    let initial = SatelliteStreamRequest {
+        satellite_id,
+        stream_id: stream_id.unwrap_or_default(),
+        resume_stream_message_ack_id: stream_resume_id.unwrap_or_default(),
+        ..Default::default()
+    };
 
-        tx_ctx.cancelled().await;
-    });
+    match tx.send(initial).await {
+        Ok(_) => info!("sent initial stream request"),
+        Err(err) => error!(?err, "failed to send initial stream request"),
+    };
+}
 
-    tokio::time::sleep(Duration::from_secs(30)).await;
+fn on_message(msg: SatelliteStreamResponse, results: &mut StreamResult) {
+    results.stream_id = Some(msg.stream_id);
 
-    tx_handle.await;
-    rx_handle.await;
+    match msg.response {
+        Some(msg) => match msg {
+            Response::ReceiveTelemetryResponse(msg) => {
+                let frames = msg.telemetry.len();
+                let bytes = msg
+                    .telemetry
+                    .iter()
+                    .map(|frame| frame.data.len())
+                    .sum::<usize>();
 
-    Ok(())
+                debug!(
+                    frames,
+                    bytes,
+                    plan = msg.plan_id,
+                    satellite = msg.satellite_id,
+                    groundstation = msg.ground_station_id,
+                    message = msg.message_ack_id,
+                    "received telemetry message"
+                );
+
+                results.frames += frames;
+                results.bytes += bytes;
+                results.complete = frames == 1 && bytes == 0;
+                results.stream_resume_id = Some(msg.message_ack_id);
+            }
+            Response::StreamEvent(event) => {
+                debug!("received event message");
+                match event.event {
+                    Some(Event::CommandSent(event)) => {
+                        debug!(?event, "received CommandSent event (currently unsupported)")
+                    }
+                    Some(Event::PlanMonitoringEvent(event)) => {
+                        debug!(?event, "received PlanMonitoringEvent event")
+                    }
+                    None => warn!("received empty stream event message"),
+                }
+            }
+        },
+        None => {}
+    };
 }
 
 pub async fn stream(args: Args) -> anyhow::Result<()> {
     // Create an OAuth2 token source to produce bearer tokens for authentication
     let tokens = token_source(args.key, &args.url).await?;
 
-    // Create a reusable channel for gRPC connections
     let channel = Endpoint::new(args.url.clone())?
         .user_agent("streamcli")?
         .connect()
         .await?;
 
+    let client = StellarStationServiceClient::new(channel)
+        .max_decoding_message_size(5 * 1024 * 1024)
+        .max_encoding_message_size(5 * 1024 * 1024);
+
     let ctx = CancellationToken::new();
 
     let mut tasks = Vec::new();
-    for i in 0..args.count {
-        let task = tokio::spawn(one_stream(
-            i,
-            ctx.child_token(),
-            StreamConfig {
-                channel: channel.clone(),
-                bearer: tokens.token().await?.access_token,
-                satellite: args.satellite_id.clone(),
-            },
-        ));
+    for _ in 0..args.count {
+        let task = match args.reconnect {
+            true => tokio::spawn(stream_with_reconnect(
+                ctx.child_token(),
+                tokens.token().await?.access_token,
+                client.clone(),
+                args.satellite_id.clone(),
+            )),
+            false => tokio::spawn(stream_attempt(
+                ctx.child_token(),
+                tokens.token().await?.access_token,
+                client.clone(),
+                args.satellite_id.clone(),
+                args.reconnect_stream_id.clone(),
+                args.reconnect_message_index.clone(),
+            )),
+        };
         tasks.push(task);
     }
 
+    // Wait for all stream tasks to complete
     for task in tasks {
         if let Err(err) = task.await? {
             tracing::warn!(?err, "failed to wait for task?");
